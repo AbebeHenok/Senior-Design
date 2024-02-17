a14ce5c69531211fa1cadfbf2814f20d63579262
Henok Abebe <henokabebe@Henoks-Air.eduroam.gmu.edu>
2024-02-16 21:57:40 -0500

    Received code started, syntax fix

--- SD_main.py
+++ SD_main.py
@@ -37,7 +37,7 @@ flagBit = 0
 flagCounter = 0
 
 #Global Variables for received string. Initialized to an empty string.
-recievedString = ""
+receivedString = ""
 
 #Global Variable for transmission string. Initialized to an empty string.
 stringToTransmit = ""
@@ -54,9 +54,11 @@ warning = False
 #Reset bit: If reset flag is high, Hazard, flagBit, and flagCounter will be reset to default values
 reset = 0
 
+# Global Direction variable
+direction = 0
     
 def main_thread():
-    global lock, flagBit, flagCounter, recievedString, stringToTransmit, onHighway,hazard,warning
+    global lock, flagBit, flagCounter, receivedString, stringToTransmit, onHighway,hazard,warning, direction
     
     highWay: bool = False #initializing highway variable to be false.
     
@@ -84,7 +86,7 @@ def main_thread():
         
     #alternative, use if statements to compare degrees
     #GETTING DIRECTION - GPS METHOD
-    prevTangle = tAngle;
+    prevTangle = tAngle
     tAngle = 1 #fromGPS OR MANUAL
     #IF STATEMENTS to SET DIRECTION
     
@@ -93,8 +95,8 @@ def main_thread():
         onHighway = True # if speed > 55, store direction EW or NS
            #GETTING DIRECTION - COORDINATE METHOD
         direction = 0 #0 = South, 1 = North, 2 = West, 3=East
-        longDiff = currLon - prevLon;
-        latDiff = currLat - prevLat;
+        longDiff = currLon - prevLon
+        latDiff = currLat - prevLat
         if math.abs(longDiff) >= math.abs(latDiff): # Going East/West
            
             if longDiff >= 0:#going East
@@ -112,7 +114,7 @@ def main_thread():
    #direction = 0 #0 = east, 1 = west, etc
     
     if(onHighway):#start reciever thread and initialize sensor lists
-        _thread.start_new_thread(receiver_thread,())
+        _thread.start_new_thread(reciever_transmitter_thread,())
                 
         DEQUE_SIZE = 5.0
         
@@ -186,81 +188,103 @@ def main_thread():
                 if zGyro_avg > math.abs(0.4):
                     #transmit
                     warning = True
-                if direction == 0 || direction == 1:#stored at start of active mode: north or south
-                    if currDirection  > 1 && speed > 10: # going east or west. speed check to ignore jitter at low speed.
-                        onHighway = false;
+                if direction == 0 or direction == 1:#stored at start of active mode: north or south
+                    if currDirection  > 1 and speed > 10: # going east or west. speed check to ignore jitter at low speed.
+                        onHighway = False
                         direction = currDirection    
-                elif direction == 2 || direction == 3 && speed > 10: # going east or west. speed check to ignore jitter at low speed.
-                    if currDirection  < 2 && speed > 10:   #going north and south
-                        onHighway = false;
+                elif direction == 2 | direction == 3 and speed > 10: # going east or west. speed check to ignore jitter at low speed.
+                    if currDirection  < 2 and speed > 10:   #going north and south
+                        onHighway = False
                         direction = currDirection
                 if hazard: #warn users w LED n buzzer
                     hazard = False
             time.sleep(period)
             
 def reciever_transmitter_thread():
+    global direction
     uart =UART(0,baudrate = 9600,stop = 1 ,tx = Pin(0),rx = Pin(1))
     #uart = UART.init(baudrate=9600, bits=8, parity=None, stop=1, Tx = 0, Rx = 1)
     sensorReading = "1,0,30.0031,20.1241,W" # global string
 
     uart.write(bytes("AT+MODE=TEST", "utf-8"))  #ATTEMPTING AT+MODE=TEST AUTO
     print("Checking.. AT+MODE=TEST")
-    time.sleep(2);
-    uart.write(bytes("AT+TEST=RXLRPKT, "utf-8")) #set in reciever mode
+    time.sleep(2)
+    uart.write(bytes("AT+TEST=RXLRPKT", "utf-8")) #set in reciever mode
     
     while True:# keep recieving
         line = uart.readline() #checking for messages from Lora module
         if warning:
             break
-        elif line != null: #if there is a message
-            recievedString += uart.readline(); #compile/add to a string
-        elif recievedString != null && line == null: #uart message is finished
-            #analyze recievedString message
+        elif line != None: #if there is a message
+            receivedString += uart.readline(); #compile/add to a string
+        elif receivedString != None and line == None: #uart message is finished
+            #analyze receivedString message
             left = '"3C'  #PARSING STRING
             right = '3E"'
             dataRead = re.search(
-                r"" + left + "(.*?)" + right + "", recievedString
+                r"" + left + "(.*?)" + right + "", receivedString
             ).group(1)
             clearstring = binascii.unhexlify(dataRead).decode("utf8")
             print(clearstring)
-            letter_list = clearstring.split(",")
-            flag_str = letter_list[0]
-            hazard_str = letter_list[1]
-            gpsX_str = letter_list[2]
-            gpsY_str = letter_list[3]
-            dir_str = letter_list[4]
-            print(“flag: “ + flag_str)
-            print(“hazard: “ + hazard_str)
-            print(“gpsX: “ + gpsX_str)
-            print(“gpsY: “ + gpsY_str)
-            print(“dir: “ + dir_str)
-
-            #still strings, need to convert + stick in object type cast
-
-            # determine direction from the transmitted packet
-
-            # check if traveling in the same direction
-
-            # check if we're in proximity of hazard (1.5 miles)
-            #TEST CHANGE#########################################################
-
-            # determine hazard // check hazard counter, determine  
-
-            # 
             
-            recievedString == null
+            letter_list = clearstring.split(",")    
+            flag_str = letter_list[0]           #Flag bit value       
+            hazard_str = letter_list[1]         #Hazard bit value
+            gpsX_str = letter_list[2]           # GPS val
+            gpsY_str = letter_list[3]           # GPS val
+            dir_str = letter_list[4]            # Direction-  0: South, 1: North, 2: West, 3: East 
+
+            #Printing strings of our received values
+            print("flag " +  flag_str)
+            print("hazard:" + hazard_str)
+            print("gpsX: " + gpsX_str)
+            print("gpsY: " + gpsY_str)
+            print("dir: " + dir_str)
+
+            ############### still strings, need to convert + stick in object type cast ##############
+
+            dir_received = int(dir_str)                 # Direction in integer format  --- local
+            flagBit_received = int(flag_str)             # Flag bit in integer format  --- local
+            hazard_received = int(hazard_str)            # hazard bit in integer format --- local
+
+            lat_received = float(gpsX_str)                   # Latitude value
+            lon_received = float(gpsY_str)                   # Longtitude value
             
-        #if recievedString.length > 0, 
-  #  if hazard:
-    b = "AT+TEST=TXLRSTR "+bytes(binascii.hexlify(stringToTransmit.encode('utf-8'))).decode()
-    b = bytes(b,'utf-8') 
-    print(b.decode())
-    warning = False
+        
+            ################# determine direction from the received packet ########################
+
+            if(dir_received != direction):  # 
+                pass
+            else:
+                continue
+                # check if the transmitter's location is behind or in front -- use GPS latitude and/or longtitude
+
+                # If we are moving closer toward the hazard -- check hazard array
+                # If hazard array doesn't contain incident, save data into array
+                # else ignore
+
+                # us being the car in front: lat = 160
+                # hazard received with lat = 120
+
+                # Depending on direction:
+                # if(rec_lat - curLat < 0) :
+                # this for us.
+                # else we're moving away
+
+            # Transmission Code:      
+            # if hazard:
+    # b = "AT+TEST=TXLRSTR "+bytes(binascii.hexlify(stringToTransmit.encode('utf-8'))).decode()
+    # b = bytes(b,'utf-8') 
+    # print(b.decode())
+    # warning = False
+            
+    #uart.write(b);  
+    # while True:
+    # time.sleep(1);
+    # print(uart.readline());
+
+
+if __name__ == "__main__":
+    main_thread()
+
     
-     #   uart.write(b);  
-     #   while True:
-      #      time.sleep(1);
-       #     print(uart.readline());
-
-
-main_thread()
