65278fce123e6048ff8483a3a52f3e220a10e10f
David-M-Ngo <113471793+David-M-Ngo@users.noreply.github.com>
2024-03-27 16:48:08 -0400

    Update migration3.py

--- migration3.py
+++ migration3.py
@@ -2,7 +2,7 @@
 #
 # Authors - Henok Abebe, David Ngo, Jhaysonel  Gueverra, Nhu Pham, Mohmmed Rahman, Anthony C Cruz
 # 
-# Discription - This program is implemented to run on Raspberry Pi Pico with a LoRa module, 6 DOF Accelerometer/Gyroscope,
+# Description - This program is implemented to run on Raspberry Pi Pico with a LoRa module, 6 DOF Accelerometer/Gyroscope,
 # and a GPS module. The program is meant to collect data from the GPS and Accelerometer/Gyroscope modules
 # and analyze drivers behavior on a highway. If the Accelerometer/Gyroscope module detects sudden changes, it will
 # raise the flagBit and increment the flagCounter. When a flagBit is raised, the program goes into the comm function
