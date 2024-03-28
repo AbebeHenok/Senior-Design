dda795497ca00791d11b11bf987962b52a4e916b
AbebeHenok <154027504+AbebeHenok@users.noreply.github.com>
2024-03-28 14:17:44 -0400

    Update main_3inch5.py

--- lib/main_3inch5.py
+++ lib/main_3inch5.py
@@ -10,8 +10,7 @@ LCD_MOSI = 11
 LCD_MISO = 12
 LCD_BL   = 13
 LCD_RST  = 15
-TP_CS    = 16
-TP_IRQ   = 17
+
 
 class LCD_3inch5(framebuf.FrameBuffer):
 
@@ -35,13 +34,13 @@ class LCD_3inch5(framebuf.FrameBuffer):
         self.rst = Pin(LCD_RST,Pin.OUT)
         self.dc = Pin(LCD_DC,Pin.OUT)
         
-        self.tp_cs =Pin(TP_CS,Pin.OUT)
-        self.irq = Pin(TP_IRQ,Pin.IN)
+#         self.tp_cs =Pin(TP_CS,Pin.OUT)
+#         self.irq = Pin(TP_IRQ,Pin.IN)
         
         self.cs(1)
         self.dc(1)
         self.rst(1)
-        self.tp_cs(1)
+#         self.tp_cs(1)
         self.spi = SPI(1,6_000_000)
         print(self.spi)  
         self.spi = SPI(1,baudrate=40_000_000,sck=Pin(LCD_SCK),mosi=Pin(LCD_MOSI),miso=Pin(LCD_MISO))
@@ -217,160 +216,3 @@ class LCD_3inch5(framebuf.FrameBuffer):
             pwm.duty_u16(65535)
         else:
             pwm.duty_u16(655*duty)
-
-    def touch_get(self): 
-        if self.irq() == 0:
-            self.spi = SPI(1,4_000_000,sck=Pin(LCD_SCK),mosi=Pin(LCD_MOSI),miso=Pin(LCD_MISO))
-            self.tp_cs(0)
-            X_Point = 0
-            Y_Point = 0
-            for i in range(0,3):
-                self.spi.write(bytearray([0XD0]))
-                Read_date = self.spi.read(2)
-                time.sleep_us(10)
-                X_Point=X_Point+(((Read_date[0]<<8)+Read_date[1])>>3)
-                
-                self.spi.write(bytearray([0X90]))
-                Read_date = self.spi.read(2)
-                Y_Point=Y_Point+(((Read_date[0]<<8)+Read_date[1])>>3)
-
-            X_Point=X_Point/3
-            Y_Point=Y_Point/3
-            
-            self.tp_cs(1) 
-            self.spi = SPI(1,40_000_000,sck=Pin(LCD_SCK),mosi=Pin(LCD_MOSI),miso=Pin(LCD_MISO))
-            Result_list = [X_Point,Y_Point]
-            #print(Result_list)
-            return(Result_list)
-if __name__=='__main__':
-
-    LCD = LCD_3inch5()
-    LCD.bl_ctrl(100)
-    LCD.fill(LCD.BLACK)
-    LCD.show_up()
-    LCD.fill(LCD.BLACK)
-    LCD.show_down()
-    if LCD.rotate == 0 or LCD.rotate == 180: #Determining the display direction
-        #color BRG     
-        LCD.fill(LCD.WHITE)
-        LCD.fill_rect(60,75,200,30,LCD.RED)
-        LCD.text("Raspberry Pi Pico",90,87,LCD.WHITE)
-        display_color = 0x001F
-        LCD.text("3.5' IPS LCD TEST",90,127,LCD.BLACK)
-        for i in range(0,12):      
-            LCD.fill_rect(i*20+35,170,30,120,(display_color))
-            display_color = display_color << 1
-        LCD.show_up()
-        
-        while True:
-            get = LCD.touch_get()
-            if get != None: 
-                X_Point = int((get[1]-430)*480/3270)
-                if(X_Point>480):
-                    X_Point = 480
-                elif X_Point<0:
-                    X_Point = 0
-                Y_Point = 320-int((get[0]-430)*320/3270)
-                if LCD.rotate == 0 :
-                    if(X_Point<120):
-                        LCD.fill(LCD.WHITE)
-                        if(Y_Point<90):
-                            LCD.fill_rect(10,150,75,50,LCD.RED)
-                            LCD.text("Button0",20,170,LCD.WHITE)
-                        elif(Y_Point<160):
-                            LCD.fill_rect(85,150,75,50,LCD.RED)
-                            LCD.text("Button1",100,170,LCD.WHITE)
-                        elif(Y_Point<240):
-                            LCD.fill_rect(160,150,75,50,LCD.RED)
-                            LCD.text("Button2",175,170,LCD.WHITE)
-                        else:
-                            LCD.fill_rect(235,150,75,50,LCD.RED)
-                            LCD.text("Button3",250,170,LCD.WHITE)
-                else:
-                    if(X_Point>360):
-                        LCD.fill(LCD.WHITE)
-                        if(Y_Point<90):
-                            LCD.fill_rect(235,150,75,50,LCD.RED)
-                            LCD.text("Button3",250,170,LCD.WHITE)                     
-                        elif(Y_Point<160):
-                            LCD.fill_rect(160,150,75,50,LCD.RED)
-                            LCD.text("Button2",175,170,LCD.WHITE)                         
-                        elif(Y_Point<240):
-                            LCD.fill_rect(85,150,75,50,LCD.RED)
-                            LCD.text("Button1",100,170,LCD.WHITE)
-                        else:
-                            LCD.fill_rect(10,150,75,50,LCD.RED)
-                            LCD.text("Button0",20,170,LCD.WHITE)
-            else :
-               LCD.fill(LCD.WHITE)                     
-               LCD.text("Button0",20,170,LCD.BLACK)
-               LCD.text("Button1",100,170,LCD.BLACK)
-               LCD.text("Button2",175,170,LCD.BLACK)
-               LCD.text("Button3",250,170,LCD.BLACK)   
-            LCD.show_down()  
-            time.sleep(0.1)
-
-    else:
-        #color BRG     
-        LCD.fill(LCD.WHITE)
-        LCD.fill_rect(140,5,200,30,LCD.RED)
-        LCD.text("Raspberry Pi Pico",170,17,LCD.WHITE)
-        display_color = 0x001F
-        LCD.text("3.5' IPS LCD TEST",170,57,LCD.BLACK)
-        for i in range(0,12):      
-            LCD.fill_rect(i*30+60,100,30,50,(display_color))
-            display_color = display_color << 1
-        LCD.show_up()
-        
-        while True:      
-            get = LCD.touch_get()
-            if get != None: 
-                X_Point = int((get[1]-430)*480/3270)
-                if(X_Point>480):
-                    X_Point = 480
-                elif X_Point<0:
-                    X_Point = 0
-                Y_Point = 320-int((get[0]-430)*320/3270)
-# When the rotation is 90°, remove the comment below, and the touch rotation at 270° can be annotated
-# 90° touch rotation is enabled by default
-                if(Y_Point<120):
-                    LCD.fill(LCD.WHITE)
-                    if(X_Point<120):
-                        LCD.fill_rect(360,60,120,100,LCD.RED)
-                        LCD.text("Button3",400,110,LCD.WHITE)
-                    elif(X_Point<240):
-                        LCD.fill_rect(240,60,120,100,LCD.RED)
-                        LCD.text("Button2",270,110,LCD.WHITE)
-                    elif(X_Point<360):
-                        LCD.fill_rect(120,60,120,100,LCD.RED)
-                        LCD.text("Button1",150,110,LCD.WHITE)          
-                    else:               
-                        LCD.fill_rect(0,60,120,100,LCD.RED)
-                        LCD.text("Button0",20,110,LCD.WHITE)
-# When the rotation is 270°, remove the comment below, and the touch rotation at 90° can be annotated
-                                 
-#                 if(Y_Point>220):
-#                     LCD.fill(LCD.WHITE)
-#                     if(X_Point<120):
-#                         LCD.fill_rect(0,60,120,100,LCD.RED)
-#                         LCD.text("Button0",20,110,LCD.WHITE)
-#                     elif(X_Point<240):
-#                         LCD.fill_rect(120,60,120,100,LCD.RED)
-#                         LCD.text("Button1",150,110,LCD.WHITE)
-#                     elif(X_Point<360):
-#                         LCD.fill_rect(240,60,120,100,LCD.RED)
-#                         LCD.text("Button2",270,110,LCD.WHITE)
-#                     else:
-#                         LCD.fill_rect(360,60,120,100,LCD.RED)
-#                         LCD.text("Button3",400,110,LCD.WHITE)           
-            else :
-               LCD.fill(LCD.WHITE)
-               LCD.text("Button0",20,110,LCD.BLACK)
-               LCD.text("Button1",150,110,LCD.BLACK)
-               LCD.text("Button2",270,110,LCD.BLACK)
-               LCD.text("Button3",400,110,LCD.BLACK)         
-            LCD.show_down()  
-            time.sleep(0.1)
-               
-
-
