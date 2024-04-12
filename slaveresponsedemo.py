8044223659f9c29cf2c57fc7653d3caaebfd45de
David-M-Ngo <113471793+David-M-Ngo@users.noreply.github.com>
2024-04-12 12:41:58 -0400

    Update slaveresponsedemo.py

--- slaveresponsedemo.py
+++ slaveresponsedemo.py
@@ -1,9 +1,9 @@
 values = []
-populatelist()
+populatelist() #get an o, hazard type, and 2 digits before displaying it the first time.
 isFirstDigit = True
 
 while True:
-    string = get() 
+    string = get() #either update numbers or f, which means clear list and repopulate it again before writing to display once more.
     if(string == '0' or string == '1' or string == '2' or string == '3' or string == '4' or string == '5' or string == '6' or string == '7' or string == '8' or string == '9' or string == '0')
         lcd(string, True)
         isFirstDigit.toggle()
