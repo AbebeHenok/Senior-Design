b830d9a9084e31e60f83fce6c443977150a35c47
David-M-Ngo <113471793+David-M-Ngo@users.noreply.github.com>
2024-04-12 12:37:47 -0400

    Update slaveresponsedemo.py

--- slaveresponsedemo.py
+++ slaveresponsedemo.py
@@ -16,11 +16,19 @@ while True:
 
 def populatelist(self):
     global value
-    while(len(value) < 6): #before list has receieved o, hazard type, dig1, dig2 AT LEAST ONCE
+    while(len(value) < 4): #before list has receieved o, hazard type, dig1, dig2 AT LEAST ONCE
         string = get()
         vallen = len(value)
-        if( (string == 'o' and vallen == 0) or ( (string == 'l' or string == 'r' or string == 'b') and vallen == 1) or ( (string == '0' or string == '1' or string == '2' or string == '3' or string == '4' or string == '5' or string == '6' or string == '7' or string == '8' or string == '9' or string == '0') and (vallen == 2 or vallen == 3)) )
-            value.append(string)
+        if(vallen == 0):
+            if(string == 'o'):
+                value.append(string)
+        elif(vallen == 1):   
+            if (string == 'l' or string == 'r' or string == 'b')
+                value.append(string)
+        elif(vallen == 2 or vallen == 3):   
+            if (string == 'l' or string == 'r' or string == 'b') and vallen == 1) or ( (string == '0' or string == '1' or string == '2' or string == '3' or string == '4' or string == '5' or string == '6' or string == '7' or string == '8' or string == '9' or string == '0')
+                value.append(string)
+
     backlighton()
     writewarning()
     lcd(val[2], True)
